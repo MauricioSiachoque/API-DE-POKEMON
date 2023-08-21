c7f712f6ad6c200fe309e487afe374897758c64c
mauricio siachoque <jhonattanramos19@hotmail.com>
2023-08-20 21:41:00 -0500

    me actualizo a las clases

--- api/controllers.js
+++ api/controllers.js
@@ -11,4 +11,5 @@ export async function buscarPokemon(){
         let dataParseada = await data.json();                   // para parsear, se convierte a jeison 
 
         console.log(dataParseada);
+        return dataParseada.results;               // results: es para validar sola una cierta informacion y no toda
 }
--- api/main.js
+++ api/main.js
@@ -4,4 +4,29 @@ saludar()
 buscarPokemon()
 
 
+let root = document.getElementById("root");                  // para leer en el html o llamarlo 
+
+async function render(){
+    let pokemones = await buscarPokemon(); 
+
+    let html ="";
+
+    pokemones.forEach((pokemon) => {  
+      let cardpokemon =  `<div>                                               
+                             <span>${pokemon.name}</span>                       
+                            <span>${pokemon.url}</span>                        
+                        </div>`;
+
+                                                                        // let cardpokemon: es para leer cada pokemon //
+                                                                        //  para tomar el objeto pokemon, y los busca las caractersicticas para renderizar en pantalla
+                                                                        //  para tomar el objeto pokemon, y los busca las caractersicticas para renderizar en pantalla
+    html += cardpokemon;          
+    });
+
+     root.innerHTML += html;
+
+}
+
+render();
+
 /// main es el renderizado
